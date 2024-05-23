fb7fb92309230f4affb8bf15357751be4cacbea0
Toshiki_Ogihara <toshiki.ogihara14@gmail.com>
2024-05-23 07:44:46 +0000

    ユーザー登録の入力パラメータについて制限を追加。 - 名前は文字数15文字まで。 - メールアドレスは「~@~.~」の形式のみ。 - パスワードは半角英数、半角数字をそれぞれ1文字以上含める かつ 更に文字数が8~32まで。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+\z/.freeze()
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze()
   
-  has_secure_password
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, uniqueness: true, format: { 
+    with: VALID_EMAIL_REGEX
+  }
+  
+  has_secure_password validations: false
+  validates :password, format: {
+    with: VALID_PASSWORD_REGEX, 
+    message: "パスワードは半角英字と半角数字それぞれ1文字以上含める必要があります。", 
+  }
+  validates_length_of :password, in: 8..32, message: "パスワードは8文字以上32文字以下に設定してください。"
+  validates_confirmation_of :password, allow_blank: true
+  
 end
