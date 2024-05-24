54b6fe5526eef6f12082b164f57c77365516274c
Toshiki_Ogihara <toshiki.ogihara14@gmail.com>
2024-05-24 03:39:18 +0000

    ログイン機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
@@ -6,4 +7,8 @@ Rails.application.routes.draw do
   get 'pages/link'
   
   resources :users
+  
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
