9ac5eecff93be4e2763de6b9d6a7f2374a5dffbb
Toshiki_Ogihara <toshiki.ogihara14@gmail.com>
2024-05-24 06:12:26 +0000

    ApplicationControllerとApplicationHelperで同じコードがあるため、ヘルパー側のコードを削除しコントローラ側でヘルパーとして呼び出せるように変更。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
   
